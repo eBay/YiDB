773a0385a28c92797a442834f12427b3be931fe2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-17 20:42:15 +0800

    Delete sdk directory

--- sdk/java-typesafe-api/src/main/java/com/ebay/cloud/cms/typsafe/metadata/model/RepositoryOption.java
+++
@@ -1,9 +0,0 @@
-package com.ebay.cloud.cms.typsafe.metadata.model;
-
-/**
- * @author liasu
- *
- */
-public class RepositoryOption {
-
-}
--- sdk/java-typesafe-api/src/main/java/com/ebay/cloud/cms/typsafe/restful/IJsonBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.ebay.cloud.cms.typsafe.restful;
-
-/**
- * @author liasu
- *
- */
-public interface IJsonBuilder {
-    String buildJson(Object object);
-}
