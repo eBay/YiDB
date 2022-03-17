090be243b057d52bc4225d79b621de169070aa6f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-17 20:39:34 +0800

    Delete buildcronus directory

--- buildcronus/package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf target
-mkdir target
-curl -sS 'https://raw.githubusercontent.com/yubin154/cronusagent/master/agent/scripts/cronus_package/package.sh' | DIR=. appName=yims version=0.1.1 platform=x86_ubuntu bash
-mv *.cronus* target/
-
